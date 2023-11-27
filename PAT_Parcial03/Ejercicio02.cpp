bfed69e5fd3c8bf6e83ad9b0bc1b59fbacb2f1d5
fer-herreraflores <ferdiherrera@hotmail.com>
2023-11-27 00:04:30 -0600

    P3

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,31 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
 
+static std::string getDnaSequence(Node<char>* current) {
+    std::string sequence;
+    for (int i = 0; i < 10 && current != nullptr; ++i) {
+        sequence += current->value;
+        current = current->next;
+    }
+    return sequence;
+}
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
-    return nullptr;
+    std::vector<std::string>* result = new std::vector<std::string>();
+    std::unordered_map<std::string, int> sequenceCount;
+
+    Node<char>* current = head;
+
+    while (current != nullptr) {
+        std::string sequence = getDnaSequence(current);
+        sequenceCount[sequence]++;
+
+        if (sequenceCount[sequence] == 2) {
+            result->push_back(sequence);
+        }
+
+        current = current->next;
+    }
+
+    return result;
 }
